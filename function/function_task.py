cc266514cf4d53bed2ada9c0a8bc21cf6a09750f
ajithsky <ajithsky311@gmail.com>
2025-02-27 21:04:34 +0530

    first commit

--- function/function_task.py
+++ function/function_task.py
@@ -1,3 +1,4 @@
+#1
 def number(num):
     if num%2 == 0:
         print("Even.")
@@ -5,3 +6,26 @@ def number(num):
         print("odd")
 
 prnit(number(5))
+
+
+
+
+
+#2
+
+def string(str):
+    return str[::-1]
+
+print(string("hello"))
+
+
+
+
+#3
+
+
+def num(n):
+    return sum(n)
+
+
+print(num([1,2,3,4,5]))
