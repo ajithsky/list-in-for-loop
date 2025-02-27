005e499ddc41fcfc254198a9108c910b21be2a5d
ajithsky <ajithsky311@gmail.com>
2025-02-27 08:15:14 +0530

    first commit

---
+++ function/function_task.py
@@ -0,0 +1,7 @@
+def number(num):
+    if num%2 == 0:
+        print("Even.")
+    else:
+        print("odd")
+
+prnit(number(5))
---
+++ list_for loop/list_in_for_loop.py
@@ -0,0 +1,39 @@
+#for loop
+
+
+for i in range(1,8):
+    print(i)
+
+
+
+lis = [1,2,3,4,5,6]
+
+for i in lis:
+    if i%2 != 0:
+        print(i)
+
+
+
+lis = [1,10,3,8,5,9]
+
+for i in range(1,len(lis),2):
+    print(lis[i])
+
+
+
+lis = [1,10,3,8,5,9]
+
+for i in range(0,len(lis),2):
+    print(lis[i])
+
+
+lis = [1,2,3,4,5,5,6]
+
+for i in lis:
+    if i%2 !=0:
+        print(i)
+
+
+
+
+
